a7a53706ae13a2cc875d452392a431a3540939f5
nikhil-31 <nkl07ba@gmail.com>
2023-03-04 16:02:44 +0530

    Added error generation code for frontend and backend service

--- reference-app/backend/app.py
+++ reference-app/backend/app.py
@@ -27,6 +27,22 @@ jaeger_tracer = config.initialize_tracer()
 tracing = FlaskTracing(jaeger_tracer, True, app)
 
 
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv["message"] = self.message
+        return rv
+
+
 @app.route("/")
 def homepage():
     return "Hello World"
@@ -47,6 +63,45 @@ def add_star():
     new_star = star.find_one({"_id": star_id})
     output = {"name": new_star["name"], "distance": new_star["distance"]}
     return jsonify({"result": output})
+
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
+@app.route("/403")
+def status_code_403():
+    status_code = 403
+    raise InvalidUsage(
+        "Raising status code: {}".format(status_code), status_code=status_code
+    )
+
+
+@app.route("/404")
+def status_code_404():
+    status_code = 404
+    raise InvalidUsage(
+        "Raising status code: {}".format(status_code), status_code=status_code
+    )
+
+
+@app.route("/500")
+def status_code_500():
+    status_code = 500
+    raise InvalidUsage(
+        "Raising status code: {}".format(status_code), status_code=status_code
+    )
+
+
+@app.route("/503")
+def status_code_503():
+    status_code = 503
+    raise InvalidUsage(
+        "Raising status code: {}".format(status_code), status_code=status_code
+    )
 
 
 if __name__ == "__main__":
